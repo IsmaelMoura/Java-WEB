fab7be248b3c8f1b512d69507adabe3b6f5979c6
Ismael de Sousa <ismaeldesousa2014@outlook.com>
2022-04-29 22:12:25 -0300

    Comentários

---
+++ novo/Agenda/build/classes/controller/Controller.java
@@ -0,0 +1,94 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.DAO;
+import model.JavaBeans;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet(urlPatterns = { "/Controller", "/main", "/inserir" })
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
+
+	/**
+	 * Default constructor.
+	 */
+	public Controller() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		// Teste Conexão
+		// DAO dao = new DAO();
+		// dao.testeConexao();
+		String action = request.getServletPath();
+		System.out.println(action);
+		if (action.equals("/main")) {
+			contatos(request, response);
+		} else if (action.equals("/inserir")) {
+			novoContato(request, response);
+		} else
+			response.sendRedirect("index.html");
+
+	}
+
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// response.sendRedirect("agenda.jsp");
+		// receber dados da instância JavaBeans
+		ArrayList<JavaBeans> lista = dao.listaContatos();
+		// teste
+		/*
+		 * for (int i = 0; i < lista.size(); i++) {
+		 * System.out.println(lista.get(i).getIdcon());
+		 * System.out.println(lista.get(i).getNome());
+		 * System.out.println(lista.get(i).getFone());
+		 * System.out.println(lista.get(i).getEmail()); }
+		 */
+		
+		// atribui o apelino par ao objeto
+		request.setAttribute("contatos", lista);
+		
+		// para onde será despachado
+		RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+		
+		// despacha para o caminho declarado anteriormente
+		rd.forward(request, response);
+
+	}
+
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.contato.setNome(request.getParameter("nome"));
+		this.contato.setFone(request.getParameter("fone"));
+		this.contato.setEmail(request.getParameter("email"));
+		this.contato.setObsPath(request.getParameter("obsPath"));
+
+		dao.inserirContato(contato);
+
+		response.sendRedirect("main");
+
+	}
+
+}
